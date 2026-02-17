d5e2794122e21b8f3efd737a63d1dc4d8152ed38
Nappier <nappierbug@gmail.com>
2026-02-18 02:41:10 +0300

    Add MainLayout component with Sidebar and children

---
+++ MainLayout.tsx
@@ -0,0 +1,5 @@
+      <Sidebar />
+      <main className="ml-64 min-h-screen transition-all duration-300">
+        <div className="p-6 lg:p-8">{children}</div>
+        <div className="p-4 sm:p-6 lg:p-8 max-w-[1400px]">{children}</div>
+      </main>
